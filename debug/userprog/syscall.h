2047c013db302adb310b0e4cf7aea076982e806a
EBUCILONG <940246862@qq.com>
2018-12-09 13:54:36 +0800

    bo wen jia you

---
+++ debug/userprog/syscall.h
@@ -0,0 +1,20 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include "lib/kernel/list.h"
+#include "vm/mmfile.h"
+
+struct file_descriptor{
+	int fid;
+	int owner;
+	struct file* sys_file;
+	struct list_elem elem;
+};
+
+void syscall_init (void);
+// struct lock file_lock;
+/* be the struct keep track of all the opened file */
+struct list fd_list;
+
+
+#endif /* userprog/syscall.h */
---
+++ debug/vm/mmfile.h
@@ -0,0 +1,52 @@
+#ifndef MMFILE_H
+#define MMFILE_H
+
+#include "userprog/process.h"
+#include <debug.h>
+#include <inttypes.h>
+#include <round.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "userprog/gdt.h"
+#include "userprog/pagedir.h"
+#include "userprog/tss.h"
+#include "filesys/directory.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "threads/flags.h"
+#include "threads/init.h"
+#include "threads/interrupt.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "devices/timer.h"
+#include "userprog/syscall.h"
+#include "vm/frame.h"
+
+struct lock file_lock;
+
+struct mmfile_entry
+{
+  int mapid;
+  struct file* mapped_file;
+  void* addr;
+  int pg_num;
+  struct hash_elem elem;
+};
+
+// Function pointers
+unsigned mmf_hash_func (const struct hash_elem *e, void * aux UNUSED);
+bool mmf_descend (const struct hash_elem * a, const struct hash_elem * b, void* aux UNUSED);
+
+// insert an entry into the table
+int mmf_insert (struct file* f, void *addr, int length);
+
+void mmf_free_entry (struct mmfile_entry* mmf);
+
+// Destroy the hash table
+void mmf_destroy_table (struct hash *mmfiles);
+
+#endif
